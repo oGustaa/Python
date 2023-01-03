76fa84ca3faf8674220064b69b75b8a1119eb277
Luiz Gustavo de Oliveira <ogusta321@gmail.com>
2023-01-03 17:14:31 -0300

    commited

---
+++ codeone.py
@@ -0,0 +1,46 @@
+print("Hello World!")
+
+num1=10
+num2=5
+res=0
+nome1="Luiz"
+nome2="Gustavo"
+idade=18
+
+
+print("Meu nome é " + nome1 + " " + nome2)
+
+if num1 > num2 and nome1 == "Luiz":
+    print("Maior!")
+else:
+    print("Menor!")
+
+
+"""Brincando com Python rsrs"""
+
+def ps():
+
+    
+
+
+    nomee = input("Olá, qual é o seu nome?")
+    print("Seja Bem-Vindo a nossa biblioteca " + nomee+"!")
+
+    idadee = input("Antes de apresentarmos nossas coleções, nos diga sua idade...")
+
+    idadee = int(idadee)
+    if idadee >= idade:
+        
+        print("Temos as seguintes coleções")
+    else:
+        print("Temos essas coleções")
+
+        
+ps()
+
+
+acao = ['Velozes e furiosos', 'Homem-aranha']
+
+print(acao)
+
+
