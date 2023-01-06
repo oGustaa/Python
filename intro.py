5420a449d686ba35cdb58f663af8522a64c9e41c
Luiz Gustavo de Oliveira <ogusta321@gmail.com>
2023-01-06 20:41:46 -0300

    newcommit

---
+++ intro.py
@@ -0,0 +1,46 @@
+### dando introdução ao python pelo w3schools
+### https://www.w3schools.com/python/python_intro.asp
+### https://www.w3schools.com/python/python_syntax.asp
+
+M = "Masculino"
+
+F = "Feminino"
+
+O = "Outro"
+
+print("********Introduzindo********")
+
+nome = input("Qual seu nome?") ### -> interagindo com usuário ~ os dados serão 
+                                ### armazenados nesta var.
+
+idade = input("Agora, sua idade") ### °..
+
+
+
+def Genero():
+    gen = input("Nos apresente seu gênero")
+
+
+gen = input("Nos diga seu gênero inserindo M para masculino e F para feminino. Para outro, digite O")
+if gen == M:
+    gen == "Masculino"
+
+elif gen == F:
+    gen == "Feminino"
+
+elif gen == O:
+    Genero()
+
+
+else:
+    print("Digite novamente!")
+        
+
+
+
+
+
+    
+
+
+
